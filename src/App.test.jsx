48cad7e2856b4d5b7f5fff0d6d3a12d36df8ef60
gagor02 <gagor0202@gmail.com>
2026-04-12 16:24:28 -0700

    prueba de github actions

---
+++ src/App.test.jsx
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest';
+
+describe('Validación del Trabajo Final', () => {
+  it('Debe pasar las pruebas de CI/CD', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
+
+// Me quede en el paso 3 de gemini
